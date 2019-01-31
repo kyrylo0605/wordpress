47dc130ac465a89f66b42ccc2f5eee846e6637da
Kyrylo Grebennyk <kyrylo06051986@gmail.com>
2019-01-31 04:10:17 +0200

    update elementor

--- wp-content/plugins/elementor/assets/js/editor-modules.js
+++ wp-content/plugins/elementor/assets/js/editor-modules.js
@@ -1,4 +1,4 @@
-/*! elementor - v2.4.4 - 24-01-2019 */
+/*! elementor - v2.4.5 - 30-01-2019 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
